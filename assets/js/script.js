fa7b467b4f26238e98412aadf0489376f0fab98a
Zhunixy <kauan8643@gmail.com>
2023-10-19 18:57:57 -0300

    Adicionei contador de score!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,43 @@
-const mario = document.querySelector('.mario'); //Constante que adiciona a class 'mario'
-const pipe = document.querySelector('.pipe'); //Constante que adiciona a class 'pipe' (cano)
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const scoreElement = document.querySelector('#score'); // Elemento para exibir a pontuação
+let score = 0;
 
-const jump = () => { //Constante referente ao evento de pular do Mario
-    mario.classList.add('jump'); //Adiciona a class 'jump' (pular) que faz o Mario ir para cima
-
-    setTimeout(( ) => { //Intervalo de tempo
- 
-        mario.classList.remove('jump'); //Retira o class 'pular' do Mario, fazendo pular 
-
-    }, 500); //500ms
+const jump = () => {
+    mario.classList.add('jump');
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 500);
 }
 
-const loop = setInterval(() => {  //intervelo entre o cano e o Mario que resulta em game-over
-    const pipePosition = pipe.offsetLeft; //constante referente a posição esquerda do cano
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ''); //Constante que muda a posição do mario e pega qualquer propriedade css
+const checkCollision = () => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    //If referente ao final do jogo
-    if (pipePosition <= 120 && marioPosition < 80) { //Se a posição do cano for menor ou igual a 120px | E se a posição do mario for menor que 80 px cima e baixo
-        pipe.style.animation = 'none'; //Tira a animação de pulo
-        pipe.style.left = `${pipePosition}px`; //Para o Mario na posição do cano
-    
-        mario.style.animation = 'none'; //para a animação
-        mario.style.bottom = `${marioPosition}px`; //define que a propriedade 'bottom' do mario vai a mesma quando ele enconstar no cano, ele não vai voltar para 0
-    
-        mario.src = 'assets/img/game-over.png' //Quando o mario morrer vai trocar a imagem para a cena de morte
-        mario.style.width = '75px'; //Altera o tamanho do png do mario morto
-        mario.style.marginLeft = '50px'  //Altera a margem entre o png mario morto e o cano
-   
-        clearInterval(loop); //Para de rodar a constante loop
+    if (pipePosition <= 120 && marioPosition < 80) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+        mario.src = 'assets/img/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+        clearInterval(loop);
+        resetGame(); // Chama a função para reiniciar o jogo
+    } else {
+        if (pipePosition < 0) {
+            updateScore(); // Chama a função para atualizar a pontuação
+        }
     }
+}
 
-}, 10)
+const updateScore = () => {
+    score++;
+    scoreElement.textContent = score;
+}
 
-document.addEventListener('keydown', jump); //Evento que adiciona pulo ao clicar na seta 
+let loop = setInterval(() => {
+    checkCollision();
+}, 10);
+
+document.addEventListener('keydown', jump);
